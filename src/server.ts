292c1ff3640d7ea570a19d124855f7cb87596d06
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-06-24 07:55:34 -0300

    new routes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
 import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(3000, () => console.log("🚀 Server is running at port 3000"));
